bef50abe6dd44faedcd38eb413763cd4844963de
MagedElfar <maged.1992.me@gmail.com>
2026-05-20 11:23:19 +0300

    couter speed

--- frontend/js/animations.js
+++ frontend/js/animations.js
@@ -1158,7 +1158,7 @@
   counters.forEach(el => {
     const target = parseFloat(el.dataset.counter);
     const decimals = parseInt(el.dataset.decimals || "0", 10);
-    const duration = parseFloat(el.dataset.duration || "4");
+    const duration = parseFloat(el.dataset.duration || "3.5");
 
     console.log("duration = ", duration)
     if (reduceMotion || !ScrollTrigger) {
